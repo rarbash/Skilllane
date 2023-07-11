718b1ff2d1c142e69dc547655d2febacaec7081e
rarbash <gai190060@gmail.com>
2023-07-12 02:59:02 +0700

    feat: add env example

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1'
+  baseURL: process.env.REACT_APP_BACKEND_URL || ''
 });
 
 instance.interceptors.request.use((request) => {
